e491a8a2665afe8e87e150cb708dd8fac1e4f06b
Yasuhiro Naito <ynaito@sfc.keio.ac.jp>
2012-08-18 22:24:39 +0900

    CytoplasmAssignmentProcess: Restore Volume Dynamics

--- CytoplasmAssignmentProcess.cpp
+++ CytoplasmAssignmentProcess.cpp
@@ -125,8 +125,9 @@ LIBECS_DM_CLASS( CytoplasmAssignmentProcess, Process )
 
 		_SizeN_A = getSuperSystem()->getSizeN_A();
 
-//		Vt->setValue( _Vt );
+		// E-Cell calculates with number-of-molecule base during simBio calc with conc base.
 		_Vt = Vi->getValue() + Vn_L;
+		Vt->setValue( _Vt );  // simBio: org.simBio.bio.terashima_et_al_2006.function.TotalVolume
 		
 		Volume_ratio->setValue( _Vt * Vt0i );  // simBio: org.simBio.bio.terashima_et_al_2006.experiment.VolumeRatio
 		
